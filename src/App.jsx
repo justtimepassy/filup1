dd6d0012ca02d6aaa43e69909686df67f1ae86e6
Manish Karthik Vutlapalli <manishkarthikvutlapalli@Manishs-MacBook-Air.local>
2024-08-24 23:23:34 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='app'>
+        <h4>Upload Notes</h4>
+        <input type="text" />
+
+
+        </div>
+    </>
+  )
+}
+
+export default App
